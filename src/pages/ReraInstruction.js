41d8b82c03a835f9402500a23b429918fb7309de
Karan96Kaushik <karankshk96@gmail.com>
2024-07-09 15:07:55 +0100

    minor

--- src/pages/ReraInstruction.js
+++ src/pages/ReraInstruction.js
@@ -26,45 +26,50 @@ const ReraInstruction = () => (
 			<CardContent> 
 
 				<Grid container>
+					<Grid sx={{paddingLeft:20, paddingBottom:5}} item xs={12}>
+						<Typography color='textPrimary' onClick={window.postMessage({ hi:'a' })}>
+							Status Check : <span id='statusCheck'>Not Installed</span>
+						</Typography>
+					</Grid>
 					<Grid item xs={12}>
-						<a href="https://github.com/user-attachments/files/16141251/chrome.zip" download >
+						<a href="https://github.com/user-attachments/files/16144464/en-chrome-ext.zip" download >
 							<Typography gutterBottom>
-									1. Download Chrome Extension
+								1. Download Chrome Extension
 							</Typography>
 						</a>
 					</Grid>
 					<Grid item xs={12}>
 						<Typography color='textPrimary' gutterBottom>
-									2. Extract zip file contents
+								2. Extract zip file contents
 						</Typography>
 					</Grid>
 					<Grid item xs={12}>
 						<Typography color='textPrimary' gutterBottom>
-									3. Open "Manage Extensions" from top right options menu
+								3. Open "Manage Extensions" from top right options menu
 						</Typography>
 					</Grid>
 					<Grid item xs={12}>
 						<Box sx={{ paddingLeft: '50px', // Adjust the indent size as needed
-								maxWidth: 500, // Optional: Constraint on the image size
-								overflow: 'hidden' }}>
-						<img src="/images/RERA-manage-entensions.png" alt="Random" style={{ width: '100%', height: 'auto' }} />
+							maxWidth: 500, // Optional: Constraint on the image size
+							overflow: 'hidden' }}>
+							<img src="/images/RERA-manage-entensions.png" alt="Random" style={{ width: '100%', height: 'auto' }} />
 						</Box>
 					</Grid>
 					<Grid sx={{paddingTop:3}} item xs={12}>
 						<Typography color='textPrimary' gutterBottom>
-									4. Select "Load Unpacked" option
+							4. Select "Load Unpacked" option
 						</Typography>
 					</Grid>
 					<Grid  item xs={12}>
 						<Box sx={{ paddingLeft: '50px', // Adjust the indent size as needed
-								maxWidth: 500, // Optional: Constraint on the image size
-								overflow: 'hidden' }}>
+							maxWidth: 500, // Optional: Constraint on the image size
+							overflow: 'hidden' }}>
 						<img src="/images/RERA-load.png" alt="Random" style={{ width: '100%', height: 'auto' }} />
 						</Box>
 					</Grid>
 					<Grid sx={{paddingTop:3}} item xs={12}>
 						<Typography color='textPrimary' gutterBottom>
-									5. Select extracted folder
+							5. Select extracted folder
 						</Typography>
 					</Grid>
 				</Grid>
