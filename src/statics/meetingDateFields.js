72a32d0e862113a140e6c4069f2da87ad6d1c136
Karan96Kaushik <karankshk96@gmail.com>
2024-08-29 00:49:49 +0100

    meetings cal

--- src/statics/meetingDateFields.js
+++ src/statics/meetingDateFields.js
@@ -2,7 +2,7 @@ export default {
     texts: [
         {label:"Title", id:"title", isRequired:true},
         {label:"Meeting Date", id:"meetingDate", type: 'date', isRequired:true},
-        {label:"Remarks", id:"remarks"},
+        {label:"Remarks", id:"remarks", type:'array'},
         // {label:"Next Hearing Remarks", id:"remarksNextHearing"},
     ],
     checkboxes: []
